20946e3b3345446e84c0f45856f8a5653bc14752
Sahil Parsaniya <sahil.parsaniya270@gmail.com>
2024-07-30 23:24:18 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Card } from './components/Card.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 rounded-xl p-4 mb-4'>Hello React + Vite + Tailwind</h1>
+      <Card />
+    </>
+  )
+}
+
+export default App
